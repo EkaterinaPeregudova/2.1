d60ba4765073e8869871638695c62735bd09fb05
katerina-kis <katerina-kis@yandex.ru>
2021-03-31 15:23:05 +0300

    первый тест v2

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -13,7 +14,7 @@ public class OrderTest {
         open("http://0.0.0.0:9999/");
         ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Пупкин Вася");
-        inputs.find(Condition.attribute("type","tel")).setValue("+790977777777");
+        inputs.find(Condition.attribute("type","tel")).setValue("+71234567890");
         $(".checkbox__box").click();
         $(".button").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
