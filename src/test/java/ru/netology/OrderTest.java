42a0fef4f21128dcad058aee623bf680b5df486e
katerina-kis <katerina-kis@yandex.ru>
2021-03-31 13:15:38 +0300

    первый тест

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -1,17 +1,21 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class OrderTest {
     @Test
     void shouldSubmitRequest() {
         open("http://0.0.0.0:9999/");
-    SelenideElement form =$("[data-test-id=callback-form]");
-    form.$("[data-test-id=name] input").setValue("Василий");
-    form.$("[data-test-id=phone] input").setValue("+79270000000");
-    form.$("[data-test-id=agreement]").click();
-    form.$("[data-test-id=submit]").click();
-    $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Пупкин Вася");
+        inputs.find(Condition.attribute("type","tel")).setValue("+790977777777");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
