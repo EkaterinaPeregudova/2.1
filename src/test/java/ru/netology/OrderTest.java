c739aa032c71f7680f31b80bac4a6038c7014fcb
katerina-kis <katerina-kis@yandex.ru>
2021-04-06 14:06:40 +0300

    имя латиницей

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,13 @@ public class OrderTest {
         $(".checkbox__box").click();
         $(".button").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void LatinizaName() {
+        open("http://0.0.0.0:9999/");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Vasilii Pupkin");
+        $(".button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 }
