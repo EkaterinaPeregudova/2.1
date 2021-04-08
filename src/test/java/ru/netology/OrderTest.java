e7e44c59abc163243b929aca4de922bc23b978b9
katerina-kis <katerina-kis@yandex.ru>
2021-04-08 11:57:36 +0300

    остальные тесты

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -27,4 +27,41 @@ public class OrderTest {
         $(".button").click();
         $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
+    @Test
+    void shouldWriteNumberTwelveSymbols(){
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Пупкин Вася");
+        inputs.find(Condition.attribute("type","tel")).setValue("+790926809090");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldWriteNumberWithoutPlus() {
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Пупкин Вася");
+        inputs.find(Condition.attribute("type","tel")).setValue("89092680909");
+        $(".checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    void shouldFieldsEmpty(){
+        open("http://localhost:9999");
+        $(".button").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldCheckboxEmpty(){
+        open("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
+        inputs.find(Condition.attribute("type","text")).setValue("Пупкин Вася");
+        inputs.find(Condition.attribute("type","tel")).setValue("+79092680909");
+        $(".button").click();
+        $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
 }
